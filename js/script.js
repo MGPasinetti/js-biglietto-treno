e5967d509c1ed0e930a869410b7daa120b15d241
MGPasinetti <mg.pasinetti@gmail.com>
2022-02-21 16:00:41 +0100

    testing

--- js/script.js
+++ js/script.js
@@ -7,8 +7,17 @@ va applicato uno sconto del 40% per gli over 65.
 L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo). Questo richiederà un minimo di ricerca.
 */
 
+let age1 = prompt (`Età 1:`)
+let age2 = prompt (`Età 2:`)
+let outputEle = document.getElementById(`my-output`);
 
-
+if (age1 > age2) {
+    outputEle.innerHTML = `Età 1 maggiore`
+} else if (age1 < age2) {
+    outputEle.innerHTML = `Età 2 maggiore`
+} else {
+    outputEle.innerHTML = `Età uguali`
+}
 
 
 
